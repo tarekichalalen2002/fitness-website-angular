f2c014430443a61430308be0538e3d2ebc42bdd7
tarekichalalen2002 <t_ichalalen@estin.dz>
2024-08-19 15:51:06 +0200

    fixing routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeNavbarComponent } from './components/home-navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeNavbarComponent, FooterComponent],
+  imports: [RouterOutlet, HomeNavbarComponent, FooterComponent, HomeComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
